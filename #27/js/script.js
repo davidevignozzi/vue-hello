6d81e270864c4a7b7117ce003e5dc7f281a481af
Davide Vignozzi <davide.vignozzi@gmail.com>
2021-03-28 17:17:44 +0200

    #27

---
+++ #27/js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#root',
+  
+    data:{
+      message: 'Hello World',
+      pic: 'img/logo.png'
+    }
+  
+  
+  })
+  // vue extension chrome
+  Vue.config.devtools = true;
+  
